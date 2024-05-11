f507a00b9bbf532dbbe2bedc2c86ce5641c1dc6b
Faizul Islam <152944531+FaizzE@users.noreply.github.com>
2024-05-11 21:57:45 +0530

    Add files via upload

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "faiz"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN(Not a Number)
+// true => 1; false => 0
+
+let isLoggesdIn = "node Basics/conversionOperation"
+
+let booleanIsLoggedIn = Boolean(isLoggesdIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "faiz" => true
+
+let randomNumber = 98
+
+let stringNumber = String(randomNumber)
+console.log(typeof stringNumber);
