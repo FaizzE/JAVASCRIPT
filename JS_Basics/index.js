42e150fc39bb88612debb07130ba3915a4c987d3
Faizul Islam <152944531+FaizzE@users.noreply.github.com>
2024-08-07 15:52:29 +0530

    Add files via upload

---
+++ JS_Basics/index.js
@@ -0,0 +1,25 @@
+console.log('Hello, Ye External JS Hai');
+
+let a = 5
+
+a = 10
+console.log(a);
+
+let lastName = 'JavaScript';
+console.log(lastName);
+
+lastName = 3;
+console.log(lastName);
+
+let num = 2;
+
+switch(num) {
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    default: console.log('D');
+}
+
