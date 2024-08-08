fdcc318f206878d3de7f1e7d9bb864b62dea7496
Faizul Islam <152944531+FaizzE@users.noreply.github.com>
2024-08-08 20:24:20 +0530

    Add files via upload

---
+++ Objects/index.js
@@ -0,0 +1,27 @@
+console.log('Objecs');
+
+// Objects Creation
+
+let rectangle = {
+    length: 1,
+    breadth: 2,
+
+    draw: function() {
+        console.log('draw');
+    }
+
+
+};
+
+let rectangle1  = {
+    length: 1,
+    breadth: 2,
+
+    draw: function() {
+        console.log('draw');
+    }
+
+
+};
+
+console.log(rectangle.length);
