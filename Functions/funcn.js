4e3092b30eba31b15d42d829d01fe60a66533a0c
Faizul Islam <152944531+FaizzE@users.noreply.github.com>
2024-08-25 18:40:38 +0530

    Update funcn.js

--- Functions/funcn.js
+++ Functions/funcn.js
@@ -41,15 +41,51 @@ function sum(a,b) {
 let ans = sum(1,2,3,4,5,6);
 // console.log(ans); 
 
+/* ======================= rest operator ===================== */
+
+function summ(...args){
+    console.log(args);
+}
+
+// sum();
+
 function addn(num,val,...args){ // rest parameter
     console.log(args);
 }
 
 // addn(1,2,3,4,5,6);
 
-/* =================== Default Parameter */
+
+/* ======================== Default Parameter ================== */
+
 function interest(p,r=6,y=9){
     return p*r*y/100;
 }
 
 // console.log(interest(1000,undefined,8));
+
+
+/* ==================== GETTER , SETTER ======================= */
+
+// Getter -> for access properties
+// Setter -> for change or mutate properties
+
+let person = {
+    fName : 'Faizul',
+    lName : 'Islam',
+
+    get fullName() {
+        return `${person.fName}  ${person.lName}`;
+    },
+
+    set fullName(value) {
+        let parts = value.split(' ');
+        this.fName = parts[0];
+        this.lName = parts[1];
+    }
+};
+
+// console.log(person);
+
+person.fullName = 'Faiz Arshad';
+console.log(person.fullName);
