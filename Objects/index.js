3e40f0fc2128b22d243041177ae6d1d82497dfa4
Faizul Islam <152944531+FaizzE@users.noreply.github.com>
2024-09-30 21:46:47 +0530

    Update index.js

--- Objects/index.js
+++ Objects/index.js
@@ -106,18 +106,18 @@ else{
 
 
 // Object Clone
-// let src = {
-//     a:10,
-//     b:20,
-//     c:30
-// };
-// let dest = {};
+let src = {
+    a:10,
+    b:20,
+    c:30
+};
+let dest = {};
 
-// for(let key in src) {
-//     dest[key] = src[key];
-// }
+for(let key in src) {
+    dest[key] = src[key];
+}
 
-// console.log(dest);
-// src.a++;
-// console.log(dest);
+console.log(dest);
+src.a++;
+console.log(dest);
 
